09e381bc8ba0bcf1bd84ee070c51cbacdb93e69d
Kayleigh <kayleighalexander05@gmail.com>
2025-09-03 20:46:42 +0100

    Advanced control flow challenge added, comparing two strings.

---
+++ CodeCademy_Lessons/Adv_control_flow/2_same_name.py
@@ -0,0 +1,18 @@
+    """take 2 names as strings and identify if they are the same
+    """
+
+
+def same_name(your_name, my_name):
+    """_summary_
+
+    Args:
+        your_name (string): _description_
+        my_name (string): _description_
+
+    Returns:
+        boolean: true if names are the same
+    """
+  if (your_name == my_name):
+    return True
+  else:
+    return False
