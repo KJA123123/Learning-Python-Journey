eb8ecd71f04eb1b10d9783dae46ff7f3f0a1670f
Kayleigh <kayleighalexander05@gmail.com>
2025-09-03 22:09:33 +0100

    OOP classes, constructors and destructors

---
+++ CodeCademy_Lessons/OOP/classes_objects.py
@@ -0,0 +1,19 @@
+"""intro to OOP
+    """
+# Class's encapsulate information and functions as a blueprint for objects.
+
+
+class Dog:
+    """Introductory to dog class
+    """
+    # dog class
+    pass
+
+# Objects
+# These are instances of classes,
+# these contain everything from the class
+# its initated from.
+
+
+Archie = Dog()
+print(Archie)
---
+++ CodeCademy_Lessons/OOP/constuctors_destructors.py
@@ -0,0 +1,26 @@
+"""constructor / destructor functions to create/delete objects
+    """
+
+# Constructors
+# Its common for classes to contain their own init function
+# An init class will create a class schedule every time the
+# class is instatated to create a new object
+
+
+class ClassSchedule:
+    def __init__(self, course):
+        self.course = course
+
+# calling itself using self.
+
+    first = ClassSchedule('Chemistry')
+    print(first.course)
+
+# Destructors
+
+    def __del__(self):
+        print('You successfully deleted your schedule')
+
+
+sched = ClassSchedule('Chemistry')
+del sched
