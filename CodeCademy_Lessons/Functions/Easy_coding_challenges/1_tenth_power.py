5b4b3c25ed3cc9cb6b732cea489a1fab84ae527c
Kayleigh <kayleighalexander05@gmail.com>
2025-09-29 11:15:52 +0100

    Corrected function names

--- CodeCademy_Lessons/Functions/Easy_coding_challenges/1_tenth_power.py
+++ CodeCademy_Lessons/Functions/Easy_coding_challenges/1_tenth_power.py
@@ -3,19 +3,40 @@
     """
 
 
-def tenth_power(num):
+def Tenth_Power(num):
+    """outputs the tenth power
+
+    Args:
+        num (integer): input number
+
+    Returns:
+        integer: tenth power
+    """
     count = 1
-    ans = 1
+    result = 1 * num
 
     while count < 10:
-        ans = num*ans  # power of 1
 
-        ans = ans
+        result *= num
 
-        count = +1
-
+        count += 1
+    return result
 # Note, I forgot about ** operator doing powers.
 
 
-def example_tenth_power(num):
+def Example_Tenth_Power(num):
+    """Shorter tenth power method
+
+    Args:
+        num (int): _description_
+
+    Returns:
+        int: _description_
+    """
     return num ** 10
+
+
+print(Tenth_Power(2))
+
+
+Example_Tenth_Power(2)
